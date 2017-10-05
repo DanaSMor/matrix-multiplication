eccb10cee12632185221c5890c22e0ec52930be6
Luan Santos <luankjs@gmail.com>
2017-10-05 17:35:18 -0300

    Replaces range method to python3

--- utils.py
+++ utils.py
@@ -5,8 +5,8 @@ def makeMatrices(matrix_a, matrix_b, result, default_size):
   max_range = default_size if default_size > 10 else 9
 
   for x in range(0, default_size):
-    matrix_a.append(random.sample(xrange(max_range), default_size))
-    matrix_b.append(random.sample(xrange(max_range), default_size))
+    matrix_a.append(random.sample(range(max_range), default_size))
+    matrix_b.append(random.sample(range(max_range), default_size))
     result.append([0] * default_size)
 
 def printMatrices(matrix_a, matrix_b, result):
